1241de861f6a8a036d995e35f5f4d6209ce8a449
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-17 20:56:47 +0200

    C

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as api from './weather-api';
 
 import './style.css';
 
+let dailyForecast = {};
+
 /**
  * Render all the data in a timeStamp to a card
  * @param {Object} timeStamp
@@ -29,10 +31,60 @@ function updateCardData(timeStamp, card) {
   PRECIP.textContent = `Chance of rain: ${timeStamp.preciption}%`;
 }
 
+/**
+ * Render single day forecast
+ * @param {Array} timeStamps an array of 8 timestamps
+ */
+function renderDailyForecast(timeStamps) {
+  const cards = document.querySelectorAll('.card');
+  timeStamps.forEach((timeStamp, index) => {
+    updateCardData(timeStamp, cards[index]);
+  });
+}
+
+/**
+ * divide timestamps list by days
+ * day1 - the next 24 hours, day2 - tomorrow etc.
+ * @param {Array} timeStamps list of timestamps
+ * @returns an Object with a list of timestamps for each day
+ */
+function groupTimeStampsByDays(timeStamps) {
+  const daysList = [];
+  timeStamps.forEach((timeStamp) => {
+    const day = timeStamp.date.getDay();
+    if (!daysList.includes(day)) {
+      daysList.push(day);
+    }
+  });
+
+  const day1 = timeStamps.slice(0, 8);
+  const day2 = timeStamps.filter((timeStamp) => timeStamp.date.getDay() === daysList[1]);
+  const day3 = timeStamps.filter((timeStamp) => timeStamp.date.getDay() === daysList[2]);
+  const day4 = timeStamps.filter((timeStamp) => timeStamp.date.getDay() === daysList[3]);
+  const day5 = timeStamps.filter((timeStamp) => timeStamp.date.getDay() === daysList[4]);
+
+  return {
+    day1, day2, day3, day4, day5,
+  };
+}
+
+async function search(location) {
+  const json = await api.fetchForecastData(location);
+  const forecast = api.processForecastData(json);
+  dailyForecast = groupTimeStampsByDays(forecast.timeStamps);
+}
+
 // on load the page show the forecast for jerusalem
+
 api.fetchForecastData('Jerusalem')
   .then((json) => api.processForecastData(json))
   .then((obj) => {
     console.log(obj);
-    updateCardData(obj.timeStamps[0], document.querySelector('.card'));
+    dailyForecast = groupTimeStampsByDays(obj.timeStamps);
   });
+
+const buttons = document.querySelectorAll('.switch-forecast > button');
+buttons.forEach((button) => button.addEventListener('click', (e) => {
+  const day = e.target.classList;
+  renderDailyForecast(dailyForecast[day]);
+}));
