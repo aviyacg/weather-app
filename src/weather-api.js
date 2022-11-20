25a1e10bf8ea89d6b5d7cb6e8a1945f44df724e9
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-20 08:30:54 +0200

    Add functions fetchWeatherData ,processWeatherData

--- src/weather-api.js
+++ src/weather-api.js
@@ -1,6 +1,24 @@
 const API_KEY = '2408e0bdb33c31c4304363b4753369a6';
 
 /**
+ * Make a current weather request from openWeatherMap
+ * @param {String} location City name
+ * @returns current weather in JSON format
+ */
+export async function fetchWeatherData(location) {
+  // fetch data
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`, { mode: 'cors' });
+    // return data in JSON format
+    const responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+/**
  *  Make a 5 days forecast request from OpenWeatherMap
  * @param {string} location can be either 'City', 'City, Country', 'Country'
  * @returns {JSON} a 5 days / 3 hour forecast in JSON format - read more at https://openweathermap.org/forecast5#JSON
@@ -72,4 +90,8 @@ function processTimestamp(timeStamp) {
 export function processForecastData(json) {
   const timeStamps = json.list.map((timeStamp) => processTimestamp(timeStamp));
   return { city: json.city.name, timeStamps };
+}
+
+export function processWeatherData(json) {
+  return (processTimestamp(json));
 }
